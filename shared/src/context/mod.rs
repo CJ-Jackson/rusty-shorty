8ab22643c8c3333abe293eed242fd41b5fd3ad54
Chris Jackson <hello@cjjackson.dev>
2025-09-22 13:24:10 +0100

    clean up context error handling

--- shared/src/context/mod.rs
+++ shared/src/context/mod.rs
@@ -1,4 +1,5 @@
 use crate::config::Config;
+use crate::error::FromErrorStack;
 use error_stack::Report;
 use poem::http::StatusCode;
 use poem::{FromRequest, Request, RequestBody};
@@ -13,25 +14,6 @@ pub enum ContextError {
     RequestError,
     #[error("Other error")]
     Other,
-    #[error("Other status code")]
-    Status(StatusCode, String),
-}
-
-impl ContextError {
-    pub fn status_code(&self) -> (StatusCode, String) {
-        match self {
-            ContextError::ConfigError => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Config Error".to_string(),
-            ),
-            ContextError::RequestError => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Request Error".to_string(),
-            ),
-            ContextError::Other => (StatusCode::INTERNAL_SERVER_ERROR, "Other Error".to_string()),
-            ContextError::Status(code, v) => (*code, v.clone()),
-        }
-    }
 }
 
 pub trait FromContext: Sized + Send + Sync {
@@ -60,9 +42,10 @@ impl<'a, T: FromContext> FromRequest<'a> for Dep<T> {
             Err(_) => return Err(poem::Error::from_status(StatusCode::INTERNAL_SERVER_ERROR)),
         };
         let context = Box::pin(Context { config, req });
-        Ok(Self(T::from_context(&context).await.map_err(|e| {
-            let status_code = e.current_context().status_code();
-            poem::Error::from_string(status_code.1, status_code.0)
-        })?))
+        Ok(Self(
+            T::from_context(&context)
+                .await
+                .map_err(poem::Error::from_error_stack)?,
+        ))
     }
 }
