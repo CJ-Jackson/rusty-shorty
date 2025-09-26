43868703e9c13025036e4b29f71e023ddec5c030
Chris Jackson <hello@cjjackson.dev>
2025-09-26 19:44:33 +0100

    use if logic on error_stack_log_service

--- shared/src/log/mod.rs
+++ shared/src/log/mod.rs
@@ -13,9 +13,8 @@ pub fn init_log() {
 pub async fn log_poem_error(err: &poem::Error) {
     if let Some(log_data) = err.data::<LogData>() {
         error!("{} - {}", err.status(), &log_data.summary);
-        let error_stack_log_service: ErrorStackLogService = fetch_context()
-            .await
-            .expect("Should return error stack service");
-        _ = error_stack_log_service.log_data(log_data);
+        if let Ok(error_stack_log_service) = fetch_context::<ErrorStackLogService>().await {
+            _ = error_stack_log_service.log_data(log_data);
+        }
     }
 }
