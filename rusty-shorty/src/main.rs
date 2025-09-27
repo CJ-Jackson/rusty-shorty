317674de8da73952435032399d77c168baa5d7f2
Chris Jackson <hello@cjjackson.dev>
2025-09-27 09:18:04 +0100

    handle thread properly

--- rusty-shorty/src/main.rs
+++ rusty-shorty/src/main.rs
@@ -1,13 +1,27 @@
 use backoffice::export::{MainError, init_log};
+use error_stack::Report;
 use error_stack::fmt::ColorMode;
-use error_stack::{Report, ResultExt};
+use tokio::task::JoinHandle;
 
 #[tokio::main]
 async fn main() -> Result<(), Report<MainError>> {
     init_log();
     Report::set_color_mode(ColorMode::None);
 
-    let handle = tokio::spawn(backoffice::boot());
-    public::boot().await?;
-    handle.await.change_context(MainError::ThreadError)?
+    let backoffice_handle = tokio::spawn(backoffice::boot());
+    let public_handle = tokio::spawn(public::boot());
+    match tokio::try_join!(flatten(backoffice_handle), flatten(public_handle)) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
+    }
+}
+
+async fn flatten(
+    handle: JoinHandle<Result<(), Report<MainError>>>,
+) -> Result<(), Report<MainError>> {
+    match handle.await {
+        Ok(Ok(result)) => Ok(result),
+        Ok(Err(err)) => Err(err),
+        Err(err) => Err(Report::new(err).change_context(MainError::ThreadError)),
+    }
 }
