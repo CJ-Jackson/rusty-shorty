3825c2b402adce298ecdf9a776c7fa8b5ba39f62
Chris Jackson <hello@cjjackson.dev>
2025-10-01 07:47:13 +0100

    clean up icon

--- backoffice/src/common/icon/mod.rs
+++ backoffice/src/common/icon/mod.rs
@@ -2,30 +2,34 @@ use crate::common::embed::AssetHidden;
 use maud::{Markup, PreEscaped};
 use shared::embed::EmbedAsString;
 
+fn get_icon(name: &str) -> Markup {
+    PreEscaped(AssetHidden::get(name).as_string())
+}
+
 pub fn plus_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/plus.svg").as_string())
+    get_icon("icon/plus.svg")
 }
 
 pub fn pencil_square_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/pencil_square.svg").as_string())
+    get_icon("icon/pencil_square.svg")
 }
 
 pub fn key_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/key.svg").as_string())
+    get_icon("icon/key.svg")
 }
 
 pub fn flag_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/flag.svg").as_string())
+    get_icon("icon/flag.svg")
 }
 
 pub fn trash_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/trash.svg").as_string())
+    get_icon("icon/trash.svg")
 }
 
 pub fn no_symbol_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/no_symbol.svg").as_string())
+    get_icon("icon/no_symbol.svg")
 }
 
 pub fn document_magnifying_glass_icon() -> Markup {
-    PreEscaped(AssetHidden::get("icon/document_magnifying_glass.svg").as_string())
+    get_icon("icon/document_magnifying_glass.svg")
 }
