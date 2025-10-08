c6ccac20cc6cf264da2d8bcd30bd4e52e30807a8
Chris Jackson <hello@cjjackson.dev>
2025-10-08 09:28:58 +0100

    fix bug with js

--- backoffice/asset/embed/js/main.js
+++ backoffice/asset/embed/js/main.js
@@ -9,44 +9,44 @@ htmx.config.responseHandling = [
 
 function formatToLocalTime() {
     let elements = document.getElementsByClassName("js-date-local");
-    for (let element of elements) {
-        let date = new Date(element.innerHTML);
-        if (isNaN(date.getTime()) || date.toString() === "Invalid Date" || date.getTime() === 0) {
-            return;
+    new Promise(function () {
+        for (let element of elements) {
+            let date = new Date(element.innerHTML);
+            if (isNaN(date.getTime()) || date.toString() === "Invalid Date" || date.getTime() === 0) {
+                return;
+            }
+            element.innerHTML = date.toLocaleString();
         }
-        element.innerHTML = date.toLocaleString();
-        element.classList.remove("js-date-local");
-    }
+    }).then(function () {
+        for (let element of elements) {
+            element.classList.remove("js-date-local");
+        }
+    })
 }
 
-formatToLocalTime();
-
 async function clearNavActive() {
-    let element = document.getElementById("tag-update");
-    if (element !== null) {
-        let elements = document.getElementsByClassName("nav-item");
-        for (let element of elements) {
-            element.classList.remove("nav-item-active");
-        }
+    let elements = document.getElementsByClassName("nav-item");
+    for (let element of elements) {
+        element.classList.remove("nav-item-active");
     }
 }
 
 function addNavActive() {
     let element = document.getElementById("tag-update");
     if (element !== null) {
-        if (element.dataset.tag === undefined || element.dataset.tag === "") {
-            return;
-        }
-        let tagElement = document.getElementById(element.dataset.tag);
-        if (tagElement !== null) {
-            tagElement.classList.add("nav-item-active");
-        }
+        clearNavActive().then(function () {
+            if (element.dataset.tag === undefined || element.dataset.tag === "") {
+                return;
+            }
+            let tagElement = document.getElementById(element.dataset.tag);
+            if (tagElement !== null) {
+                tagElement.classList.add("nav-item-active");
+            }
+        });
     }
 }
 
 htmx.onLoad(function () {
     formatToLocalTime();
-    clearNavActive().then(function () {
-        addNavActive();
-    });
+    addNavActive();
 });
